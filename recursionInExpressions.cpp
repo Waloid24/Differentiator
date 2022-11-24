b8b5bc65f630a8df9cc07af9436c7634b21727fa
Waloid24 <111529670+Waloid24@users.noreply.github.com>
2022-11-24 13:40:03 +0300

    recursive descent (v.1)

---
+++ recursionInExpressions.cpp
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int getGrammar (const char * str);
+int getExpression (void);
+int getT (void);
+int getBracket (void);
+int getNumber (void);
+void removeSpaces (char * dest, const char * source);
+
+const char * s = nullptr;
+
+int main (void)
+{
+    printf ("Enter the expression\n");
+    char * string = (char *) calloc (40, sizeof(char));
+    char * strFixed = (char *) calloc (40, sizeof(char));
+
+    fgets (string, 40, stdin);
+    printf ("string = %s", string);
+
+    removeSpaces (strFixed, string);
+    printf ("strFixed = %s\n", strFixed);
+
+    int value = getGrammar (strFixed);
+    printf ("value = %d\n", value);
+}
+
+void removeSpaces (char * dest, const char * source)
+{
+    while (*source != '\0')
+    {
+        if (*source == ' ')
+        {
+            source++;
+            removeSpaces(dest, source);
+        }
+        else
+        {
+        *dest = *source;
+        dest++;
+        source++;
+        }
+    }
+}
+
+int getGrammar (const char * str)
+{
+    printf ("in getGrammar: str = %s\n", str);
+    s = str;
+    int val = getExpression();
+    if (*s == '\0')
+    {
+        printf ("ERROR\n");
+        return val;
+    }
+    return val;
+}
+
+int getExpression (void)
+{
+    int val = getT();
+    while (*s == '+' || *s == '-')
+    {
+        char op = *s;
+        s++;
+        int val2 = getT ();
+
+        if (op == '+')
+            val += val2;
+        else
+            val -= val2;
+    }
+    return val;
+}
+
+int getT (void)
+{
+    int val = getBracket ();
+    while (*s == '*' || *s == '/')
+    {
+        char op = *s;
+        s++;
+        int val2 = getBracket (); //getT
+
+        if (op == '*')
+        {
+            val *= val2;
+        }
+        else
+        {
+            val /= val2;
+        }
+    }
+    return val;
+}
+
+int getBracket (void)
+{
+    int val = 0;
+    if (*s == '(')
+    {
+        s++;
+        val = getExpression ();
+        if (*s == '(')
+        {
+            printf ("*(s+1) = %c\n", *(s+1));
+        }
+        // assert (*s == '(');
+        s++;
+    }
+    else
+    {
+        val = getNumber ();
+    }
+    return val;
+}
+
+int getNumber (void)
+{
+    int val = 0;
+    const char * sOld = s;
+    while ('0' <= *s && *s <= '9')
+    {
+        val = val*10 + *s - '0';
+        s++;
+    }
+    assert (s != sOld);
+    return val;
+}
+
+// int getDegree (void)
+// {
+//     int val = 0;
+//
+//
+// }
